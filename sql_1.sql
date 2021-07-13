3530d60651aad938e2496b09ebe4c3288b5a045f
Mehmet Yiğit Ateş <71609304+yigitatesh@users.noreply.github.com>
2021-07-14 01:27:08 +0300

    Update sql_1.sql

--- sql_1.sql
+++ sql_1.sql
@@ -14,6 +14,12 @@ WHERE (rental_rate = 0.99) AND
 	((replacement_cost = 12.99) OR (replacement_cost = 28.99));
 
 /* 4 */
-
+SELECT last_name
+FROM customer
+WHERE first_name = 'Mary';
 
 /* 5 */
+SELECT *
+FROM film
+WHERE (length <= 50) AND 
+	NOT ((rental_rate = 2.99) OR (rental_rate = 4.99));
