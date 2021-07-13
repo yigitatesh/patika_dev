d353bcd8882dae5c443e3cd71b9b0bffbffcb496
Mehmet Yiğit Ateş <71609304+yigitatesh@users.noreply.github.com>
2021-07-14 01:48:34 +0300

    Update sql_3.sql

--- sql_3.sql
+++ sql_3.sql
@@ -5,6 +5,18 @@ WHERE (country LIKE 'A%')
 	AND (country LIKE '%a');
 
 /* 2 */
-
+SELECT country
+FROM country
+WHERE country LIKE '_____%n';
 
 /* 3 */
+SELECT country
+FROM country
+WHERE country ILIKE '%t%t%t%t%';
+
+/* 4 */
+SELECT *
+FROM film
+WHERE (title LIKE 'C%')
+	AND (length > 90)
+	AND (rental_rate = 2.99);
