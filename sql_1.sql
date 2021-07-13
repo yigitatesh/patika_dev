656514850338062523941faa1df24b0f16f97d6f
Mehmet Yiğit Ateş <71609304+yigitatesh@users.noreply.github.com>
2021-07-14 01:21:46 +0300

    Update sql_1.sql

--- sql_1.sql
+++ sql_1.sql
@@ -8,7 +8,10 @@ FROM film
 WHERE (length > 60) AND (length < 75);
 
 /* 3 */
-
+SELECT *
+FROM film
+WHERE (rental_rate = 0.99) AND
+	((replacement_cost = 12.99) OR (replacement_cost = 28.99));
 
 /* 4 */
 
