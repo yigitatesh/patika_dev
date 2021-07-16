0b0e06b28c620a2ddcaa357c0f8d852d40adafa0
Mehmet Yiğit Ateş <71609304+yigitatesh@users.noreply.github.com>
2021-07-16 19:04:04 +0300

    Create sql_5.sql

---
+++ sql_5.sql
@@ -0,0 +1,21 @@
+/* 1 */
+SELECT title
+FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+/* 2 */
+SELECT title
+FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+
+/* 3 */
+SELECT *
+FROM customer
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4;
